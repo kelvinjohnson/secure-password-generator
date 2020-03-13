2c156bf36e9a874d23261b8da34610683487bbfe
Kelvin Johnson <kelvin.b.johnson@gmail.com>
2020-03-13 10:05:32 +0000

    Added Try block and if statement

--- password_generator.py
+++ password_generator.py
@@ -1,23 +1,23 @@
 import string
-from random import randint, choice
+from random import choice
 
 
 def generate_password():
-    password = ''
 
     while True:
-
         try:
-            user_input = (int(input('How many characters for you password?: ')))
+            user_input = (int(input('Enter number of characters needed for password: ')))
             characters = string.ascii_letters + string.punctuation + string.digits
             password = "".join(choice(characters) for num in range(user_input))
-            return password
+            if user_input < 6:
+                print('Your must enter a number no lower than 6')
+            else:
+                return password
 
-        except ZeroDivisionError:
-            print('Sorry, characters cannot be zero')
         except ValueError:
-            print('You entered an invalid character. Please enter a number.')
+            print('Invalid input. Please enter a number.')
 
 
-print('New password:', generate_password())
+print(f'''Password Generated Successfully: 
+{generate_password()}''')
 
