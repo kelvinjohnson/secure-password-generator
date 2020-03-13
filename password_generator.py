c3fffd824d3e5beda3873d310c98be9ab6541a36
Kelvin Johnson <kelvin.b.johnson@gmail.com>
2020-03-13 10:23:30 +0000

    moved if statement to else block

--- password_generator.py
+++ password_generator.py
@@ -9,13 +9,15 @@ def generate_password():
             user_input = (int(input('Enter number of characters needed for password: ')))
             characters = string.ascii_letters + string.punctuation + string.digits
             password = "".join(choice(characters) for num in range(user_input))
+
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+
+        else:
             if user_input < 6:
                 print('Your must enter a number no lower than 6')
             else:
                 return password
-
-        except ValueError:
-            print('Invalid input. Please enter a number.')
 
 
 print(f'''Password Generated Successfully: 
