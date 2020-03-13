260ef60e4aa709fd8a2258a99e4faaec28cb7caf
Kelvin Johnson <kelvin.b.johnson@gmail.com>
2020-03-13 00:43:23 +0000

    added code into a try block

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,19 @@
 import string
 import random
 
-def generate_password(number_of_characters):
 
-    number_of_characters = int(input("How many characters should your password be? "))
-    characters = string.ascii_letters + string.digits + string.punctuation
+def generate_password():
+
+    try:
+        number_of_characters = int(input("Please enter the number of characters needed for password: "))
+    except ZeroDivisionError:
+        print('Sorry, you must have some characters for your password')
+    except ValueError:
+        print('Sorry, you entered an invalid value. Please enter a number')
+
     password = "".join([random.choice(string.ascii_letters + string.punctuation + string.digits)])
+    for number in range(number_of_characters):
+        return password
 
 
-
-
+print(generate_password())
